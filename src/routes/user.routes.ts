36e6dc0fe1ae446d2b88512e9eb8fdd99be8a916
Jeison Azevedo <j3ison@gmail.com>
2020-04-15 20:37:14 -0300

    atualizar avatar

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
 import { hash } from 'bcryptjs';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import UpdateAvatarUserService from '../services/UpdateAvatarUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
+const upload = multer(uploadConfig);
 const usersRouter = Router();
 
 usersRouter.post('/', async (req, res) => {
@@ -21,5 +26,23 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateFile = new UpdateAvatarUserService();
+      const user = await updateFile.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
+  }
+);
 
 export default usersRouter;
