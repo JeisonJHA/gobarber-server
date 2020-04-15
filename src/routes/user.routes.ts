ec9b408ba0897cca15a23904cee0cdb710732324
Jeison Azevedo <jeisonazevedo@MacBook-Pro-de-Jeison.local>
2020-04-15 17:58:42 -0300

    criptografia

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { hash } from 'bcryptjs';
+
 import CreateUserService from '../services/CreateUserService';
 
 const usersRouter = Router();
@@ -7,7 +9,13 @@ usersRouter.post('/', async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const createUserService = new CreateUserService();
-    const user = await createUserService.execute({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+    const user = await createUserService.execute({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    delete user.password;
     return res.send(user);
   } catch (err) {
     return res.status(400).json({ error: err.message });
