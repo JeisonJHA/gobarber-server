507c99a9b77e35e4ac44bb76237f795819274249
Jeison Azevedo <j3ison@gmail.com>
2020-05-07 22:22:13 -0300

    finish testing appointment service

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -3,13 +3,16 @@ import { uuid } from 'uuidv4';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
+import { isEqual } from 'date-fns';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentsRepository implements IAppointmentRepository {
   private appointments: Appointment[] = [];
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    return this.appointments.find(appointment => appointment.date === date);
+    return this.appointments.find(appointment =>
+      isEqual(appointment.date, date)
+    );
   }
 
   public async create({
--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,5 +16,24 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
+  });
+  it('should not be able to create a new appointment with same date', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository
+    );
+
+    const date = new Date();
+    await createAppointmentService.execute({
+      date,
+      provider_id: '123123',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date,
+        provider_id: '123123',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
