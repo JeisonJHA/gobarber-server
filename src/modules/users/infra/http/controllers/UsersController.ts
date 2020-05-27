832c3deb3f8c5f5aa232adaa06b3e79d29f86e1e
Jeison Azevedo <j3ison@gmail.com>
2020-05-26 23:37:52 -0300

    remove unnecessary hash for password

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { hash } from 'bcryptjs';
 import { container } from 'tsyringe';
 import { classToClass } from 'class-transformer';
 
@@ -9,11 +8,10 @@ export default class UsersController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { name, email, password } = req.body;
     const createUserService = container.resolve(CreateUserService);
-    const hashedPassword = await hash(password, 8);
     const user = await createUserService.execute({
       name,
       email,
-      password: hashedPassword,
+      password,
     });
 
     return res.send(classToClass(user));
