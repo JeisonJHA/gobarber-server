8dbc30a66effe5c059f9ed2eebb3566673985b77
Jeison Azevedo <j3ison@gmail.com>
2020-06-21 13:54:22 -0300

    remove unnecessary fields with class-tranformer

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
@@ -18,14 +19,18 @@ export default class ListProvidersService {
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    let users = await this.cacheProvider.recover<User[]>(
+    const users = await this.cacheProvider.recover<User[]>(
       `providers-list:${user_id}`
     );
+
     if (!users) {
       users = await this.usersRepository.findAllUsers({
         exceptUserId: user_id,
       });
-      await this.cacheProvider.save(`providers-list:${user_id}`, users);
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users)
+      );
     }
     return users;
   }
