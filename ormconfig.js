c0c2d00b2e02df8c561a94432f764f5e50e93f87
Jeison Azevedo <j3ison@gmail.com>
2020-06-27 16:55:05 -0300

    use port postgres from env

--- ormconfig.js
+++ ormconfig.js
@@ -9,7 +9,7 @@ module.exports = [
     "name": "default",
     "type": "postgres",
     "host": "localhost",
-    "port": 5432,
+    "port": process.env.POSTGRES_PORT,
     "username": process.env.POSTGRES_USER,
     "password": process.env.POSTGRES_PASSWORD,
     "database": process.env.POSTGRES_DB,
