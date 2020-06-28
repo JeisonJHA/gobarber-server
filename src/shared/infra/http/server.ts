8204aed826ddaf91764a4e19ef9eb50db9836a5c
Jeison Azevedo <j3ison@gmail.com>
2020-06-28 00:01:07 -0300

    access restricted through cors

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -17,7 +17,11 @@ import '@shared/container';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.APP_WEB_URL,
+  })
+);
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
